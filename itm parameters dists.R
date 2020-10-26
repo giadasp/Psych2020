82128de9b487ccd565606bbdd09d74462d3a7302
giadasp <giada.spaccapanico@gmail.com>
2020-10-26 15:58:52 +0100

    pseudo-guessing par

--- itm parameters dists.R
+++ itm parameters dists.R
@@ -2,15 +2,16 @@ setwd("")
 data <- read.table("item_bank_3PL_SCI.csv",sep = ";",header = TRUE)
 library(ggplot2)
 library(ggpubr)
-#install.packages("hrbrthemes")
 library(tidyverse)
 library(reshape2)
 library(hrbrthemes)
+library(extrafont)
+extrafont::loadfonts()
 library(viridis)
 long <- melt(data[c("Item_ID","a","b","c")])
 
 long %>%
-  ggplot( aes(x=variable, y=value)) +
+  ggplot(aes(x=variable, y=value)) +
   geom_boxplot(outlier.size = 1) +
   scale_fill_viridis(discrete = TRUE, alpha=0.6) +
   theme_ipsum() +
@@ -18,11 +19,7 @@ long %>%
     legend.position="none",
     plot.title = element_text(size=11),
   ) +
-  theme(
-    legend.position="none",
-    plot.title = element_text(size=11)
-  ) +
-  scale_x_discrete(labels=c("discrimination","difficulty","guessing"))+
+  scale_x_discrete(labels=c("discrimination","difficulty","pseudo-guessing"))+
   ggtitle("") +
   xlab("") +
   ylab("")
